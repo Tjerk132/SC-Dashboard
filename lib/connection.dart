5752dda3d95e7154f3ae4ce45ce8360f65eb78db
Tjerk <student@embeddedfitness.nl>
2020-09-24 16:49:25 +0200

    Recommit dashboard tile division

---
+++ lib/connection.dart
@@ -0,0 +1,14 @@
+import 'package:connectivity/connectivity.dart';
+import 'package:data_connection_checker/data_connection_checker.dart';
+
+class Connection {
+
+  static Future<bool> isConnectedToInternet() async {
+    ConnectivityResult connectivityResult = await (Connectivity().checkConnectivity());
+    // user is connected via wifi or mobile data
+    if (connectivityResult != ConnectivityResult.none) {
+      return await DataConnectionChecker().hasConnection ? true : false;
+    }
+    return false;
+  }
+}
---
+++ lib/sizing.dart
@@ -0,0 +1,37 @@
+
+
+import 'package:flutter/material.dart';
+
+class Sizing {
+  static double getSize(MediaQueryData mediaQuery, double divider,
+      {double multiplier, bool reverse}) {
+    double landscapeRatio = mediaQuery.orientation == Orientation.portrait
+        ? mediaQuery.size.height / mediaQuery.size.width
+        : mediaQuery.size.width / mediaQuery.size.height;
+    double _multiplier = multiplier != null && multiplier > 0 ? multiplier : 1;
+    return reverse == null || !reverse
+        ? (mediaQuery.orientation == Orientation.portrait
+        ? mediaQuery.size.width / divider
+        : mediaQuery.size.width /
+        divider /
+        landscapeRatio *
+        _multiplier)
+        .roundToDouble()
+        : (mediaQuery.orientation == Orientation.portrait
+        ? mediaQuery.size.height /
+        divider /
+        landscapeRatio *
+        _multiplier
+        : mediaQuery.size.height / divider)
+        .roundToDouble();
+  }
+
+  static double getSizeConstraint(
+      MediaQueryData mediaQuery, BoxConstraints constraint, double divider,
+      {double multiplier, bool reverse}) {
+    double _multiplier = multiplier != null && multiplier > 0 ? multiplier : 1;
+    return reverse == null || !reverse
+        ? (constraint.biggest.width / divider * _multiplier).roundToDouble()
+        : (constraint.biggest.height / divider * _multiplier).roundToDouble();
+  }
+}
